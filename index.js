faa77bc06432292659414ba094e299a049c7d364
erguotou <erguotou525@gmail.com>
2020-05-13 18:08:30 +0800

    reset

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const request = require('./utils/request');
 
-const channel = core.getInput('channel');
-const head = core.getInput('head');
-const body = core.getInput('body');
+try {
+  const channel = core.getInput('channel');
+  const head = core.getInput('head');
+  const body = core.getInput('body');
 
-// Get the JSON webhook payload for the event that triggered the workflow
-const payload = JSON.stringify(github.context.payload, undefined, 2)
-console.log(`The event payload: ${payload}`);
-request(github.context.payload, channel, head, body).then(() => {
-  console.log('000')
-  core.setOutput('time', new Date().toISOString())
-}).catch(err => {
-  console.log(111)
-  core.setFailed(err);
-})
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+  request(github.context.payload, channel, head, body).then(() => {
+    core.setOutput('time', new Date().toISOString())
+  }).catch(err => {
+    core.setFailed(err);
+  })
+} catch (error) {
+  core.setFailed(error.message);
+}
